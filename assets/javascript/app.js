c1d516876588cb916717d6506bb22059521b923e
Tayana Jean <tayaj718@gmail.com>
2019-09-25 00:20:24 -0400

    still working on JS code having trouble making it responsive

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,8 @@
 var questions = [
     {
-        prompt: "Who was the first Robin of Teen Titans?"
+        prompt: "Who was the first Robin of Teen Titans?\n(a) Damian Wayne\n\(b) Dick Grayson\n(c) Jason Todd\n(d) Tim Drake",
+        answer: "b"
+        console.log(this);
     }
 ]
 var score = 0;
@@ -9,3 +11,7 @@ var score = 0;
 for(var i=0; i< questions.lenght; i++) {
 var response = window.prompt(questions[i].prompt)
 }
+
+const quizContainer = document.getElementById('quiz');
+const resultsContainer = document.getElementById('results');
+const submitButton = document.getElementById('submit');
