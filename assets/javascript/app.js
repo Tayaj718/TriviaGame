b80cd2bf33238bf5756d0afbee86d1fe34407e32
Tayana Jean <tayaj718@gmail.com>
2019-09-25 00:40:15 -0400

    adjusting html

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,16 +2,28 @@ var questions = [
     {
         prompt: "Who was the first Robin of Teen Titans?\n(a) Damian Wayne\n\(b) Dick Grayson\n(c) Jason Todd\n(d) Tim Drake",
         answer: "b"
-        console.log(this);
+        // console.log(this);
     }
 ]
 var score = 0;
+
+$('document').ready(function () {
+    
+}
+
+
+
+
+
+
+
+
 
 //CREATE A FOR LOOP
 for(var i=0; i< questions.lenght; i++) {
 var response = window.prompt(questions[i].prompt)
 }
 
-const quizContainer = document.getElementById('quiz');
-const resultsContainer = document.getElementById('results');
-const submitButton = document.getElementById('submit');
+// const quizContainer = document.getElementById('quiz');
+// const resultsContainer = document.getElementById('results');
+// const submitButton = document.getElementById('submit');
