9d2097c2362ded25ff17670bf10af6c74ca1ed7e
Tayana Jean <tayaj718@gmail.com>
2019-09-24 23:44:58 -0400

    added background image, and some javascript code including for loop

---
+++ assets/javascript/app.js
@@ -0,0 +1,11 @@
+var questions = [
+    {
+        prompt: "Who was the first Robin of Teen Titans?"
+    }
+]
+var score = 0;
+
+//CREATE A FOR LOOP
+for(var i=0; i< questions.lenght; i++) {
+var response = window.prompt(questions[i].prompt)
+}
